5f2f6dee8295cadea1ab2ed9285374613e431a19
Animesh Samantaray <animeshsamantaray2@gmail.com>
2026-03-30 11:42:51 +0530

    changed ready to deploy

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api', 
+    baseURL:'https://chat-bot-bcknd.vercel.app'|| 'http://localhost:3000/api', 
     withCredentials: true, 
     headers: {
         'Content-Type': 'application/json',
